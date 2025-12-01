91a28b54f56247a5303b2e730b1d4c50732bc028
Aakash <axs8965@miami.edu>
2025-12-01 14:46:35 -0500

    DONE

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BoatType"},{"p":"<Unnamed>","l":"FleetManager"},{"p":"<Unnamed>","l":"FleetOfBoats"},{"p":"<Unnamed>","l":"SingleBoat"}];updateSearchResults();
